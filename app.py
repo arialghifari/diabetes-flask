3dd3cb2dd555770ac5971da18688119df4132676
Firdaus Kuncoro <firdauskun00@gmail.com>
2023-07-07 18:43:13 +0800

    add web

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/', methods=['POST'])
 def predict():
     features = [float(x) for x in request.form.values()]
     final_features = [np.array(features)]
